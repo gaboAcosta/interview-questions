6dbda9e4f81999c7cfbb72c4148a5285c0b8dd32
Gabo Acosta <gabo.acosta624@gmail.com>
2024-01-20 12:50:59 -0700

    Adds maxCount to remove dup

--- solved/removeDuplicatesFromSortedArray.js
+++ solved/removeDuplicatesFromSortedArray.js
@@ -1,14 +1,12 @@
 
-function removeDuplicates(nums) {
+function removeDuplicates(nums, maxOccurrence = 1) {
   let k = 0
-  let lastNumber
-  for (let i = 0; i < nums.length; i++) {
-    const currentNumber = nums[i]
-    if(currentNumber !== lastNumber) {
-      nums[k] = nums[i]
-      k++
-    }
-    lastNumber = nums[i]
+  const tally = {}
+  for(const num of nums) {
+    if(tally[num] === undefined) tally[num] = { total: 0 }
+    tally[num].total++
+    if(tally[num].total > maxOccurrence) continue
+    nums[k++] = num
   }
   return k
 }
--- tests/removeDuplicatesFromSortedArray.spec.js
+++ tests/removeDuplicatesFromSortedArray.spec.js
@@ -24,4 +24,38 @@ describe('removeDuplicatesFromSortedArray', () => {
       current = nums[i]
     }
   })
+  it('Should allow a max number of repeated numbers', () => {
+    const nums = [0,0,1,1,1,2,2,3,3,4]
+    const maxDup = 2
+    const validElements = removeDuplicates(nums, maxDup)
+    expect(validElements).toEqual(9)
+    let current = nums[0]
+    let currentCount = 1
+    for(let i = 1; i < validElements; i++) {
+      if(nums[i] === current) {
+        currentCount++
+      } else {
+        currentCount = 1
+      }
+      current = nums[i]
+      expect(currentCount).toBeLessThanOrEqual(maxDup)
+    }
+  })
+  it('Should allow a max number of repeated numbers', () => {
+    const nums = [0,0,0,1,1,1,1,2,2,2,3,3,3,4,4]
+    const maxDup = 3
+    const validElements = removeDuplicates(nums, maxDup)
+    expect(validElements).toEqual(14)
+    let current = nums[0]
+    let currentCount = 1
+    for(let i = 1; i < validElements; i++) {
+      if(nums[i] === current) {
+        currentCount++
+      } else {
+        currentCount = 1
+      }
+      current = nums[i]
+      expect(currentCount).toBeLessThanOrEqual(maxDup)
+    }
+  })
 })
